08877a2f39c4c10bc233024aa586036f932601be
Thibault Goudouneix <nilmanduil@gmail.com>
2017-10-25 15:26:54 +0200

    Add progress bar in contact addition (TP Multithreading)

--- AddressBookSwift4/AddContactViewController.swift
+++ AddressBookSwift4/AddContactViewController.swift
@@ -15,9 +15,27 @@ class AddContactViewController: UIViewController {
     @IBOutlet weak var firstnameField: UITextField!
     @IBOutlet weak var lastnameField: UITextField!
     
+    @IBOutlet weak var additionProgressView: UIProgressView!
+    var progress: Float = 0
+    
     @IBAction func didPressAdd(_ sender: Any) {
         if let firstname =  firstnameField.text, let lastname = lastnameField.text {
-            self.delegate?.addContact(firstname: firstname, lastname: lastname)
+            additionProgressView.progress = 0.0
+            additionProgressView.alpha = 1
+            DispatchQueue.global(qos: .userInitiated).async {
+                while self.progress < 1 {
+                    Thread.sleep(forTimeInterval: 0.01)
+                    self.progress += 0.01
+                    
+                    DispatchQueue.main.async {
+                        self.additionProgressView.progress = self.progress
+                    }
+                }
+                
+                DispatchQueue.main.async {
+                    self.delegate?.addContact(firstname: firstname, lastname: lastname)
+                }
+            }
         }
     }
     
